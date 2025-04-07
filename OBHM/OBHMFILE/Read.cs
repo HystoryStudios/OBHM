09d4324ad9c83f797c0b2748ec6ad6b87bf2935e
HystoryStudios <s.durgrossiord68@gmail.com>
2025-04-07 15:10:28 +0200

    0.0.0.2

---
+++ OBHM/OBHMFILE/Read.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OBHM.OBHMFILE
+{
+    public class Read
+    {
+        public string Path { get; set; }
+        public Read(string path)
+        {
+            Path = path;
+            ReadFile(path);
+        }
+
+        private Dictionary<float, PaternCircle> ReadFile(string path)
+        {
+            Dictionary<float, PaternCircle> level = new Dictionary<float, PaternCircle>();
+            foreach (var line in File.ReadAllLines(path))
+            {
+                string[] item = line.Split(" ");
+            }
+            return level;
+        }
+    }
+}
